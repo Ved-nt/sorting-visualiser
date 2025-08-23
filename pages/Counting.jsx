12c6e4229e667b7be1a8a666b63da6c7b96430ff
VEDANT SHARMA <145286056+Ved-nt@users.noreply.github.com>
2025-08-24 01:41:28 +0530

    Delete pages directory

--- pages/Counting.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Counting = () => {
-  return (
-    <div>Counting</div>
-  )
-}
-
-export default Counting
--- pages/Radix.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Radix = () => {
-  return (
-    <div>Radix</div>
-  )
-}
-
-export default Radix
